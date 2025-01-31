5a82ea3162219b6e863638b423398b4a495abf1b
Bryan <bryanferrando59@gmail.com>
2025-02-01 00:44:04 +0100

    FEAT: Dockerfile, ignore and test

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,10 @@ RUN npm run build
 
 FROM nginx:alpine
 
+ARG VITE_API_URL
+
+ENV VITE_API_URL=$VITE_API_URL
+
 COPY --from=build /app/dist /usr/share/nginx/html
 
 EXPOSE 80
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import axios from "axios";
+import { config } from "./configs/config";
+import { useEffect, useState } from "react";
+
 function App() {
-  console.log(import.meta.env);
+  const [data, setData] = useState("NO DATA");
+
+  useEffect(() => {
+    axios.get(`${config.API_URL}/containers`).then((response) => {
+      setData(response.data);
+    });
+  }, []);
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-800 text-center text-white">
       <h1 className="text-3xl">ADMIN PANEL</h1>
-      <p className="mt-4">{import.meta.env.VITE_API_URL}</p>
+      <p className="mt-4">{JSON.stringify(data)}</p>
     </div>
   );
 }
